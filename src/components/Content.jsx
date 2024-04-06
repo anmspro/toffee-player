2470b6b3f7612fe8a36d3c6563af59a99d3f6ce1
Abu Noman Md Sakib <sakib.anms@gmail.com>
2024-04-06 13:18:47 +0600

    feat: implement content section of movies

---
+++ src/components/Content.jsx
@@ -0,0 +1,31 @@
+import { Fragment } from "react";
+import toffee_logo from "../assets/images/icons/toffee.png"
+import { contentList } from "../utils/data";
+
+function Content() {
+    return (
+        <Fragment className="">
+            <div className=" pl-12">
+                <div className="p-8">
+                    <img src={toffee_logo} alt="" className="ml-auto" />
+
+                    <p className="py-2 text-xl text-[#E6EEF9]">Dramas & Series</p>
+
+                    <div class="flex flex-wrap -mx-2">
+
+                        {contentList.map((item, index) => (
+                            <div className="sm:w-1/2 md:w-1/4 px-1 mb-4 cursor-pointer rounded-md">
+                                <div className="">
+                                    {/* border-4 border-[#FF3988] this will be added if selected */}
+                                    <img src={item.image} alt="Image" className="object-cover block w-full h-auto rounded-md border-4 border-transparent hover:border-4 hover:border-[#FF398880]" />
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </Fragment>
+    );
+};
+
+export default Content;
---
+++ src/utils/data.jsx
@@ -0,0 +1,104 @@
+import tv_icon from "../assets/images/icons/live_tv.svg"
+import movie_icon from "../assets/images/icons/movie.svg"
+import info_icon from "../assets/images/icons/info.svg"
+import image1 from "../assets/images/contents/1.png"
+import image2 from "../assets/images/contents/2.png"
+import image3 from "../assets/images/contents/3.png"
+import image4 from "../assets/images/contents/4.png"
+import image5 from "../assets/images/contents/5.png"
+import image6 from "../assets/images/contents/6.png"
+import image7 from "../assets/images/contents/7.png"
+import image8 from "../assets/images/contents/8.png"
+import image9 from "../assets/images/contents/9.png"
+import image10 from "../assets/images/contents/10.png"
+import image11 from "../assets/images/contents/11.png"
+import image12 from "../assets/images/contents/12.png"
+import image13 from "../assets/images/contents/13.png"
+import image14 from "../assets/images/contents/14.png"
+import image15 from "../assets/images/contents/15.png"
+import image16 from "../assets/images/contents/16.png"
+
+export const navigationItems = [
+    {
+        id: "1",
+        name: "Live TV",
+        icon: tv_icon,
+    },
+    {
+        id: "2",
+        name: "Movies",
+        icon: movie_icon,
+    },
+    {
+        id: "3",
+        name: "Info",
+        icon: info_icon,
+    },
+];
+
+export const contentList = [
+    {
+        id: 1,
+        image: image1
+    },
+    {
+        id: 2,
+        image: image2
+    },
+    {
+        id: 3,
+        image: image3
+    },
+    {
+        id: 4,
+        image: image4
+    },
+    {
+        id: 5,
+        image: image5
+    },
+    {
+        id: 6,
+        image: image6
+    },
+    {
+        id: 7,
+        image: image7
+    },
+    {
+        id: 8,
+        image: image8
+    },
+    {
+        id: 9,
+        image: image9
+    },
+    {
+        id: 10,
+        image: image10
+    },
+    {
+        id: 11,
+        image: image11
+    },
+    {
+        id: 12,
+        image: image12
+    },
+    {
+        id: 13,
+        image: image13
+    },
+    {
+        id: 14,
+        image: image14
+    },
+    {
+        id: 15,
+        image: image15
+    },
+    {
+        id: 16,
+        image: image16
+    },
+]
