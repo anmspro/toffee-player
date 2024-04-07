5cefba15c6517e847c7577117e09dc6f38c5b932
Abu Noman Md Sakib <sakib.anms@gmail.com>
2024-04-07 15:37:17 +0600

    chore: change demo video for player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,7 +3,9 @@ import ShakaPlayer from 'shaka-player-react';
 import 'shaka-player/dist/controls.css';
 
 function Player({ source }) {
-    return <ShakaPlayer className="h-[500px]" fullscreen="true" autoplay="true" src={source} />;
+    const elephantsDream = "https://ftp.itec.aau.at/datasets/DASHDataset2014/ElephantsDream/15sec/ElephantsDream_15s_onDemand_2014_05_09.mpd"
+    const bigBunny = "https://storage.googleapis.com/shaka-demo-assets/bbb-dark-truths-hls/hls.m3u8"
+    return <ShakaPlayer className="h-[500px]" fullscreen="true" autoplay="true" src={bigBunny} />;
 }
 
 export default Player;
